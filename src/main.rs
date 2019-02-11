db279d5a45460e1e9bf9517aa688b5a59d670e67
Jochen Kiemes <jochen@kiemes.de>
2019-02-11 13:39:18 +0800

    move usage into separate module

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 
 mod midi_container;
+mod usage;
 
 #[derive(Copy, Clone)]
 enum NoteState {
@@ -55,66 +56,7 @@ fn trk2col(trk: usize, key: u32) -> Color {
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let matches = App::new("Rusthesia")
-        .version(crate_version!())
-        .author(crate_authors!("\n"))
-        .about(indoc!(
-            "
-                                    Reads midi files and creates piano notes waterfall.
-
-                                    Valid key commands, while playing:
-                                        <Cursor-Left>   Transpose half tone lower
-                                        <Cursor-Right>  Transpose half tone higher
-                                        <Cursor-Up>     Go back some time
-                                        <Space>         Pause/continue playing
-                                        "
-        ))
-        .arg(
-            Arg::with_name("transpose")
-                .short("t")
-                .long("transpose")
-                .takes_value(true)
-                .default_value("0")
-                .help("Set number of note steps to transpose"),
-        )
-        .arg(
-            Arg::with_name("play")
-                .required_unless("list")
-                .short("p")
-                .long("play-tracks")
-                .takes_value(true)
-                .multiple(true)
-                .help("Output these tracks as midi"),
-        )
-        .arg(
-            Arg::with_name("show")
-                .required_unless("list")
-                .short("s")
-                .long("show-tracks")
-                .takes_value(true)
-                .multiple(true)
-                .help("Show the tracks as falling notes"),
-        )
-        .arg(
-            Arg::with_name("list")
-                .short("l")
-                .long("list-tracks")
-                .help("List the tracks in the midi file"),
-        )
-        .arg(
-            Arg::with_name("RD64")
-                .long("rd64")
-                .help("Select 64 key Piano like Roland RD-64"),
-        )
-        .arg(
-            Arg::with_name("MIDI")
-                .help("Sets the midi file to use")
-                .required(true)
-                .index(1),
-        )
-        .arg(Arg::with_name("verbose").multiple(true).short("v"))
-        .arg(Arg::with_name("debug").short("d"))
-        .get_matches();
+    let matches = usage::usage();
 
     let debug = matches.is_present("debug");
     simple_logging::log_to_stderr(if debug {
---
+++ src/usage.rs
@@ -0,0 +1,66 @@
+use clap::{crate_authors, crate_version, value_t, values_t};
+use clap::{App, Arg};
+use indoc::indoc;
+
+pub fn usage() -> clap::ArgMatches<'static> {
+    App::new("Rusthesia")
+        .version(crate_version!())
+        .author(crate_authors!("\n"))
+        .about(indoc!(
+            "
+                                    Reads midi files and creates piano notes waterfall.
+
+                                    Valid key commands, while playing:
+                                        <Cursor-Left>   Transpose half tone lower
+                                        <Cursor-Right>  Transpose half tone higher
+                                        <Cursor-Up>     Go back some time
+                                        <Space>         Pause/continue playing
+                                        "
+        ))
+        .arg(
+            Arg::with_name("transpose")
+                .short("t")
+                .long("transpose")
+                .takes_value(true)
+                .default_value("0")
+                .help("Set number of note steps to transpose"),
+        )
+        .arg(
+            Arg::with_name("play")
+                .required_unless("list")
+                .short("p")
+                .long("play-tracks")
+                .takes_value(true)
+                .multiple(true)
+                .help("Output these tracks as midi"),
+        )
+        .arg(
+            Arg::with_name("show")
+                .required_unless("list")
+                .short("s")
+                .long("show-tracks")
+                .takes_value(true)
+                .multiple(true)
+                .help("Show the tracks as falling notes"),
+        )
+        .arg(
+            Arg::with_name("list")
+                .short("l")
+                .long("list-tracks")
+                .help("List the tracks in the midi file"),
+        )
+        .arg(
+            Arg::with_name("RD64")
+                .long("rd64")
+                .help("Select 64 key Piano like Roland RD-64"),
+        )
+        .arg(
+            Arg::with_name("MIDI")
+                .help("Sets the midi file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(Arg::with_name("verbose").multiple(true).short("v"))
+        .arg(Arg::with_name("debug").short("d"))
+        .get_matches()
+}
