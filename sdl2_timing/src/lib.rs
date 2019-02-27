c04ab8f428496276850f1722eec7d8524f61aa55
Jochen Kiemes <jochen@kiemes.de>
2019-02-27 21:59:24 +0800

    frame rate detection ok for 24Hz

--- sdl2_timing/src/lib.rs
+++ sdl2_timing/src/lib.rs
@@ -55,15 +55,93 @@ pub struct Sdl2Timing<'a> {
     last_us: u64,
     stamp: Option<Instant>,
     ms_per_frame: u32,
+    us_per_frame: u32,
     base_time: Option<Instant>,
     measured: bool,
     initialized: bool,
     last_frame: u64,
+    has_vsync: bool,
     lost_frames_cnt: usize,
     measures: HashMap<&'a str,(u64,u64,u64,usize)>,
     sleep_time_stats: Vec<u32>, // millisecond resolution
 }
 impl<'a> Sdl2Timing<'a> {
+    fn measure(&mut self, canvas: &mut Canvas<Window>) {
+        let start_measure = Instant::now();
+        let mut round_us_vec = vec![];
+        let mut opt_last_round_us = None;
+        loop {
+            let elapsed = start_measure.elapsed();
+            let elapsed_us = elapsed.subsec_micros() as u64
+                            + 1_000_000*elapsed.as_secs();
+            if elapsed_us > 300_000 {
+                break;
+            }
+            if let Some(last_round_us) = opt_last_round_us {
+                let round_us = elapsed_us - last_round_us;
+                if round_us > 1_000_000/250 { // Cap at 250 Hz
+                    round_us_vec.push(round_us);
+                }
+            }
+            opt_last_round_us = Some(elapsed_us);
+            sleep(Duration::from_micros(1_000));
+            canvas.present();
+        }
+        debug!("Measured round times in us: {:?}",round_us_vec);
+        if round_us_vec.len() > 5 {
+            self.has_vsync = true;
+            round_us_vec.sort();
+            let median = round_us_vec[round_us_vec.len()/2];
+            let upper = median * 21/20;
+            let lower = median * 19/20;
+            debug!("Median value: {}",median);
+            let filtered_round_us = round_us_vec.into_iter()
+                                                .filter(|&dt| (dt < upper) && (dt > lower))
+                                                .collect::<Vec<_>>();
+            debug!("Filtered times +/-5%: {:?}",filtered_round_us);
+            self.us_per_frame = (filtered_round_us.iter().cloned().sum::<u64>()
+                                        / filtered_round_us.len() as u64) as u32;
+            debug!("Calculated frame rate= {:?} us/frame",filtered_round_us);
+        }
+        self.sample("measured");
+    }
+    /// It should be called at beginning of the main loop with the display
+    /// canvas as argument and the color, which should be used to clear the canvas.
+    ///
+    pub fn canvas_present_then_clear(&mut self, 
+                                     canvas: &mut Canvas<Window>,
+                                     color: sdl2::pixels::Color) {
+        if self.initialized {
+            self.sample("Sdl2Timing: before present and clear");
+            canvas.present();
+            self.sample("Sdl2Timing: after present, before clear");
+        }
+        else {
+            self.last_us = 0;
+            self.stamp = Some(Instant::now());
+            self.initialized = true;
+
+            self.ms_per_frame = 40;
+            if !self.measured {
+                canvas.set_draw_color(color);
+                canvas.clear();
+                canvas.present();
+                canvas.set_draw_color(color);
+                canvas.clear();
+                self.measure(canvas);
+                self.measured = true;
+            }
+        }
+        canvas.set_draw_color(color);
+        canvas.clear();
+        self.sample("Sdl2Timing: after clear");
+
+        let rem_us = 1_000;
+        let sleep_duration = Duration::new(0, rem_us as u32 * 1_000);
+        trace!("before sleep {:?}", sleep_duration);
+        std::thread::sleep(sleep_duration);
+        self.sample("sleep");
+    }
     pub fn sample(&mut self, name: &'a str) {
         if !self.measures.contains_key(&name) {
             self.measures.insert(&name, (u64::max_value(),0,0,0));
@@ -84,10 +162,17 @@ impl<'a> Sdl2Timing<'a> {
         self.measures.clear();
     }
     pub fn output(&self) {
+        if self.has_vsync {
+            println!("VSYNC is in use");
+        }
+        else {
+            println!("VSYNC not detected");
+        }
+        println!("frame rate= {} us/frame",self.us_per_frame);
         for (name, (us_min,us_sum,us_max,cnt)) in self.measures.iter() {
             println!(
-                  "min={:6.6}us avg={:6.6}us max={:6.6}us {}",
-                  us_min,us_sum/ *cnt as u64,us_max,name);
+                  "cnt={:6} min={:6.6}us avg={:6.6}us max={:6.6}us {}",
+                  cnt,us_min,us_sum/ *cnt as u64,us_max,name);
         }
         for i in 0..self.sleep_time_stats.len() {
             if self.sleep_time_stats[i] > 0 {
@@ -97,86 +182,6 @@ impl<'a> Sdl2Timing<'a> {
             }
         }
     //info!(target: EV, "Lost frames: {}", control.lost_frames_cnt());
-    }
-    /// It should be called at beginning of the main loop with the display
-    /// canvas as argument and the color, which should be used to clear the canvas.
-    ///
-    pub fn canvas_present_then_clear(&mut self, 
-                                     canvas: &mut Canvas<Window>,
-                                     color: sdl2::pixels::Color) {
-        if self.initialized {
-            self.sample("Sdl2Timing: before present and clear");
-            canvas.present();
-            self.sample("Sdl2Timing: after present, before clear");
-        }
-        else {
-            self.last_us = 0;
-            self.stamp = Some(Instant::now());
-            self.initialized = true;
-
-            self.ms_per_frame = 40;
-        }
-        canvas.set_draw_color(color);
-        canvas.clear();
-        self.sample("Sdl2Timing: after clear");
-
-        self.measured = true;
-        if !self.measured {
-            for d in vec![50_000,50_000,50_000,
-                          40_000,40_000,40_000,
-                          30_000,30_000,30_000,
-                          20_000,20_000,20_000,
-                          10_000,10_000,10_000,
-                          5_000,5_000,5_000].into_iter() {
-                let start_measure = Instant::now();
-                canvas.clear(); // Blocks for VSYNC after warm up
-                self.sample("canvas clear");
-                let elapsed_us_0 = start_measure.elapsed().subsec_micros() as u32;
-                sleep(Duration::from_micros(d));
-                self.sample("sleep");
-                let elapsed_us_1 = start_measure.elapsed().subsec_micros() as u32;
-                canvas.present(); // Blocks for VSYNC after warm up
-                self.sample("canvas present");
-                let elapsed_us_2 = start_measure.elapsed().subsec_micros() as u32;
-                println!("{}+{}+{}={}",
-                            elapsed_us_0,
-                            elapsed_us_1-elapsed_us_0,
-                            elapsed_us_2-elapsed_us_1,
-                            elapsed_us_2);
-            }
-            self.output();
-
-            // Measure framerate
-            canvas.clear();
-            canvas.present();
-            for d in 0..40 {
-                let start_measure = Instant::now();
-                sleep(Duration::from_micros(d*100));
-                canvas.present();
-                sleep(Duration::from_micros(d*100));
-                canvas.present();
-                let elapsed_us = start_measure.elapsed().subsec_micros() as u32/2;
-                println!("{} framerate={:?} mHz    {} us/frame",d,1_000_000_000/elapsed_us,elapsed_us);
-            }
-            let rem_us = 10_000;
-
-            // update stats
-            let i = rem_us as usize / 1_000;
-            while i >= self.sleep_time_stats.len() {
-                self.sleep_time_stats.push(0);
-            }
-            self.sleep_time_stats[i] += 1;
-
-            let sleep_duration = Duration::new(0, rem_us as u32 * 1_000);
-            trace!("before sleep {:?}", sleep_duration);
-            std::thread::sleep(sleep_duration);
-            self.sample("sleep");
-
-            // Sleep until next frame, then present => stable presentation rate
-            trace!("before canvas present");
-            canvas.present();
-            self.sample("canvas presented");
-        }
     }
     pub fn us_till_next_frame(&mut self) -> u32 {
         if let Some(base_time) = self.base_time.as_ref() {
